1b403d8b6bbad6192dbbdccc73a35c5c735ec456
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2025-11-09 02:55:27 +0700

    feat: create func to hash password and adjuts tag struct on response

---
+++ lib/hashpassword.go
@@ -0,0 +1,14 @@
+package lib
+
+import "github.com/matthewhartstonge/argon2"
+
+func HashPassword(password string) (string, error) {
+	config := argon2.DefaultConfig()
+
+	hash, err := config.Hash([]byte(password), nil)
+	if err != nil {
+		return "", err
+	}
+
+	return string(hash.Encode()), nil
+}
--- lib/standard.go
+++ lib/standard.go
@@ -2,6 +2,6 @@ package lib
 
 type Response struct {
 	Success bool   `json:"success" example:"true"`
-	Message string `json:"message" example:"Success message"`
+	Message string `json:"message" example:"Success/Error message"`
 	Data    any    `json:"data,omitempty"`
 }
