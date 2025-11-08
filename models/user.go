1a080bc1cde92b96f307184af169b82b5c37b0fc
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2025-11-09 02:56:09 +0700

    feat: adjust struct model user

--- models/user.go
+++ models/user.go
@@ -2,7 +2,9 @@ package models
 
 type User struct {
 	Id        int    `form:"id" swaggerignore:"true"`
-	FirstName string `form:"first_name" example:"koda"`
-	LastName  string `form:"last_name" example:"koda"`
-	Email     string `form:"email" example:"koda@mail.com"`
+	FirstName string `form:"first_name" binding:"required,min=3,max=20" example:"koda"`
+	LastName  string `form:"last_name" binding:"required,min=3,max=20" example:"koda"`
+	Email     string `form:"email" binding:"required,email" example:"koda@mail.com"`
+	Role      string `form:"role" swaggerignore:"true" example:"customer/admin"`
+	Password  string `form:"password" binding:"required,min=6" example:"koda123" format:"password"`
 }
