0b8e499bb1ce26962715404101d76009c4694140
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2025-11-09 02:55:53 +0700

    feat: create function to create user

--- controllers/usercontroller.go
+++ controllers/usercontroller.go
@@ -9,6 +9,7 @@ import (
 	"strconv"
 
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
 	"github.com/jackc/pgx/v5"
 )
 
@@ -54,7 +55,6 @@ func GetAllUser(ctx *gin.Context) {
 	var totalData int
 	err := config.DB.QueryRow(context.Background(),
 		"SELECT COUNT(*) FROM users").Scan(&totalData)
-
 	if err != nil {
 		ctx.JSON(http.StatusInternalServerError, lib.Response{
 			Success: false,
@@ -67,7 +67,6 @@ func GetAllUser(ctx *gin.Context) {
 	rows, err := config.DB.Query(context.Background(),
 		"SELECT id, first_name, last_name, email, role FROM users LIMIT $1 OFFSET $2",
 		limit, offset)
-
 	if err != nil {
 		ctx.JSON(http.StatusInternalServerError, lib.Response{
 			Success: false,
@@ -77,7 +76,6 @@ func GetAllUser(ctx *gin.Context) {
 	}
 
 	users, err := pgx.CollectRows(rows, pgx.RowToStructByName[models.User])
-
 	if err != nil {
 		ctx.JSON(http.StatusInternalServerError, lib.Response{
 			Success: false,
@@ -87,7 +85,6 @@ func GetAllUser(ctx *gin.Context) {
 	}
 
 	totalPage := (totalData + limit - 1) / limit
-
 	if page > totalPage && totalData > 0 {
 		ctx.JSON(http.StatusBadRequest, lib.Response{
 			Success: false,
@@ -155,4 +152,85 @@ func GetUserById(ctx *gin.Context) {
 			Message: "User not found",
 		})
 	}
+}
+
+// CreateUser godoc
+// @Summary      Create new user
+// @Description  Create a new user with a unique username and email
+// @Tags         users
+// @Accept       x-www-form-urlencoded
+// @Produce      json
+// @Param        user      formData  models.User true "User registration data"
+// @Param        role      formData  string false "User role" default("customer")
+// @Success      200       {object}  lib.Response{data=models.User}  "User created successfully"
+// @Failure      400       {object}  lib.Response  "Invalid request body or hash password failed"
+// @Failure      409       {object}  lib.Response  "Email or username already exists"
+// @Failure      500       {object}  lib.Response  "Failed to insert user"
+// @Router       /users [post]
+func CreateUser(ctx *gin.Context) {
+	var body models.User
+	err := ctx.ShouldBindWith(&body, binding.Form)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, lib.Response{
+			Success: true,
+			Message: err.Error(),
+		})
+		return
+	}
+
+	rows, err := config.DB.Query(context.Background(),
+		"SELECT id, first_name, last_name, email, role FROM users")
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, lib.Response{
+			Success: false,
+			Message: "Failed to query users",
+		})
+		return
+	}
+
+	users, err := pgx.CollectRows(rows, pgx.RowToStructByName[models.User])
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, lib.Response{
+			Success: false,
+			Message: "Failed to get users",
+		})
+		return
+	}
+
+	for _, user := range users {
+		if user.Email == body.Email {
+			ctx.JSON(http.StatusConflict, lib.Response{
+				Success: false,
+				Message: "Email already registered",
+			})
+			return
+		}
+	}
+
+	hashPassword, err := lib.HashPassword(body.Password)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, lib.Response{
+			Success: false,
+			Message: "Hash password failed",
+		})
+		return
+	}
+
+	body.Password = string(hashPassword)
+
+	_, err = config.DB.Exec(ctx,
+		`INSERT INTO users (first_name, last_name, email, role, password) VALUES ($1, $2, $3, $4, $5)`, body.FirstName, body.LastName, body.Email, body.Role, body.Password)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, lib.Response{
+			Success: false,
+			Message: "Failed to insert user",
+		})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, lib.Response{
+		Success: true,
+		Message: "User created successfully",
+		Data:    body,
+	})
 }
