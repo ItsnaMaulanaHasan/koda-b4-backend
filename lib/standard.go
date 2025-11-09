e71237f4ab96d8642217269e223466a45cdbfe01
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2025-11-09 11:38:23 +0700

    feat: adjust struct response, divide it into 2 types, success and error

--- lib/standard.go
+++ lib/standard.go
@@ -1,7 +1,13 @@
 package lib
 
-type Response struct {
+type ResponseSuccess struct {
 	Success bool   `json:"success" example:"true"`
-	Message string `json:"message" example:"Success/Error message"`
+	Message string `json:"message" example:"Success message"`
 	Data    any    `json:"data,omitempty"`
+}
+
+type ResponseError struct {
+	Success bool   `json:"success" example:"false"`
+	Message string `json:"message" example:"Error message"`
+	Error   string `json:"error,omitempty" example:"cause of error"`
 }
