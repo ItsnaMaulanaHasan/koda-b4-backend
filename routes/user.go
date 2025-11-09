5e3985004371dcc3b12495d7ad6d6fb4e8d4385c
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2025-11-09 17:03:27 +0700

    feat: added middleware for users

--- routes/user.go
+++ routes/user.go
@@ -2,12 +2,13 @@ package routes
 
 import (
 	"backend-daily-greens/controllers"
+	"backend-daily-greens/middlewares"
 
 	"github.com/gin-gonic/gin"
 )
 
 func SetupUserRoutes(r *gin.Engine) {
-	users := r.Group("/users")
+	users := r.Group("/users", middlewares.Auth())
 	{
 		users.GET("", controllers.GetAllUser)
 		users.GET("/:id", controllers.GetUserById)
