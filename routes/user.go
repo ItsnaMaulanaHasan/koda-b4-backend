b1a262966bb3ee38dd0dc86f2599ee5b6e8a2c87
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2025-11-09 11:44:24 +0700

    feat: add route to update user data

--- routes/user.go
+++ routes/user.go
@@ -12,5 +12,6 @@ func SetupUserRoutes(r *gin.Engine) {
 		users.GET("", controllers.GetAllUser)
 		users.GET("/:id", controllers.GetUserById)
 		users.POST("", controllers.CreateUser)
+		users.PATCH("/:id", controllers.UpdateUser)
 	}
 }
