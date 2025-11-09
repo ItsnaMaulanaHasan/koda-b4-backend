99b7dda93f27b97ed6ef91b4c5a28f41cd276ea6
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2025-11-09 11:39:39 +0700

    feat: adjust the users model, split it into UserResponse and UpdateUserRequest structs

--- models/user.go
+++ models/user.go
@@ -1,10 +1,25 @@
 package models
 
 type User struct {
-	Id        int    `form:"id" swaggerignore:"true"`
-	FirstName string `form:"first_name" binding:"required,min=3,max=20" example:"koda"`
-	LastName  string `form:"last_name" binding:"required,min=3,max=20" example:"koda"`
-	Email     string `form:"email" binding:"required,email" example:"koda@mail.com"`
-	Role      string `form:"role" swaggerignore:"true" example:"customer/admin"`
-	Password  string `form:"password" binding:"required,min=6" example:"koda123" format:"password"`
+	Id        int    `swaggerignore:"true"`
+	FirstName string `form:"first_name" binding:"required" example:"John"`
+	LastName  string `form:"last_name" binding:"required" example:"Cena"`
+	Email     string `form:"email" binding:"required,email" example:"johncena@mail.com"`
+	Role      string `form:"role" swaggerignore:"true"`
+	Password  string `form:"password" binding:"required" example:"koda123" format:"password"`
+}
+
+type UpdateUserRequest struct {
+	FirstName string `form:"first_name" binding:"required" example:"John"`
+	LastName  string `form:"last_name" binding:"required" example:"Cena"`
+	Email     string `form:"email" binding:"required,email" example:"johncena@mail.com"`
+	Role      string `form:"role" example:"customer/admin"`
+}
+
+type UserResponse struct {
+	Id        int    `json:"id"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Email     string `json:"email"`
+	Role      string `json:"role"`
 }
