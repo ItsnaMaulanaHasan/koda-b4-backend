bb5df8e562baf89cd7d03cfeed243685f264b8e8
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2025-11-10 01:13:18 +0700

    feat: create middleware admin only to filter so that only admins can access it

---
+++ middlewares/adminonly.go
@@ -0,0 +1,22 @@
+package middlewares
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func AdminOnly() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		role := c.GetString("role")
+		if role != "admin" {
+			c.JSON(http.StatusForbidden, gin.H{
+				"success": false,
+				"message": "Access forbidden: admin only",
+			})
+			c.Abort()
+			return
+		}
+		c.Next()
+	}
+}
