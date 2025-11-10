99d4fc8d177b84054974600f5fc912a2b682c082
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2025-11-10 14:22:15 +0700

    feat: added DELETE ON CASCADE

--- database/migrations/000002_create_product_related_tables.up.sql
+++ database/migrations/000002_create_product_related_tables.up.sql
@@ -11,8 +11,8 @@ CREATE TABLE "products" (
     "is_flash_sale" bool DEFAULT false,
     "stock" int CHECK ("stock" >= 0),
     "is_active" bool DEFAULT true,
-    "created_at" timestamp DEFAULT(CURRENT_TIMESTAMP),
-    "updated_at" timestamp DEFAULT(CURRENT_TIMESTAMP),
+    "created_at" timestamp DEFAULT (CURRENT_TIMESTAMP),
+    "updated_at" timestamp DEFAULT (CURRENT_TIMESTAMP),
     "created_by" int,
     "updated_by" int
 );
@@ -21,8 +21,8 @@ CREATE TABLE "product_images" (
     "id" serial PRIMARY KEY,
     "image" text NOT NULL,
     "product_id" int,
-    "created_at" timestamp DEFAULT(CURRENT_TIMESTAMP),
-    "updated_at" timestamp DEFAULT(CURRENT_TIMESTAMP),
+    "created_at" timestamp DEFAULT (CURRENT_TIMESTAMP),
+    "updated_at" timestamp DEFAULT (CURRENT_TIMESTAMP),
     "created_by" int,
     "updated_by" int
 );
@@ -31,8 +31,8 @@ CREATE TABLE "size_products" (
     "id" serial PRIMARY KEY,
     "product_id" int,
     "size_id" int,
-    "created_at" timestamp DEFAULT(CURRENT_TIMESTAMP),
-    "updated_at" timestamp DEFAULT(CURRENT_TIMESTAMP),
+    "created_at" timestamp DEFAULT (CURRENT_TIMESTAMP),
+    "updated_at" timestamp DEFAULT (CURRENT_TIMESTAMP),
     "created_by" int,
     "updated_by" int
 );
@@ -41,8 +41,8 @@ CREATE TABLE "product_categories" (
     "id" serial PRIMARY KEY,
     "product_id" int,
     "category_id" int,
-    "created_at" timestamp DEFAULT(CURRENT_TIMESTAMP),
-    "updated_at" timestamp DEFAULT(CURRENT_TIMESTAMP),
+    "created_at" timestamp DEFAULT (CURRENT_TIMESTAMP),
+    "updated_at" timestamp DEFAULT (CURRENT_TIMESTAMP),
     "created_by" int,
     "updated_by" int
 );
@@ -54,7 +54,7 @@ ALTER TABLE "products"
 ADD CONSTRAINT "fk_products_updated_by" FOREIGN KEY ("updated_by") REFERENCES "users" ("id");
 
 ALTER TABLE "product_images"
-ADD CONSTRAINT "fk_product_images_product_id" FOREIGN KEY ("product_id") REFERENCES "products" ("id");
+ADD CONSTRAINT "fk_product_images_product_id" FOREIGN KEY ("product_id") REFERENCES "products" ("id") ON DELETE CASCADE;
 
 ALTER TABLE "product_images"
 ADD CONSTRAINT "fk_product_images_created_by" FOREIGN KEY ("created_by") REFERENCES "users" ("id");
@@ -63,7 +63,7 @@ ALTER TABLE "product_images"
 ADD CONSTRAINT "fk_product_images_updated_by" FOREIGN KEY ("updated_by") REFERENCES "users" ("id");
 
 ALTER TABLE "size_products"
-ADD CONSTRAINT "fk_size_products_product_id" FOREIGN KEY ("product_id") REFERENCES "products" ("id");
+ADD CONSTRAINT "fk_size_products_product_id" FOREIGN KEY ("product_id") REFERENCES "products" ("id") ON DELETE CASCADE;
 
 ALTER TABLE "size_products"
 ADD CONSTRAINT "fk_size_products_size_id" FOREIGN KEY ("size_id") REFERENCES "sizes" ("id");
@@ -75,7 +75,7 @@ ALTER TABLE "size_products"
 ADD CONSTRAINT "fk_size_products_updated_by" FOREIGN KEY ("updated_by") REFERENCES "users" ("id");
 
 ALTER TABLE "product_categories"
-ADD CONSTRAINT "fk_product_categories_product_id" FOREIGN KEY ("product_id") REFERENCES "products" ("id");
+ADD CONSTRAINT "fk_product_categories_product_id" FOREIGN KEY ("product_id") REFERENCES "products" ("id") ON DELETE CASCADE;
 
 ALTER TABLE "product_categories"
 ADD CONSTRAINT "fk_product_categories_category_id" FOREIGN KEY ("category_id") REFERENCES "categories" ("id");
