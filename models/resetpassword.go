5a17aba89488f3e7c03386dc9e19b79ca4c85b37
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2025-11-11 01:20:25 +0700

    feat: create model for reset password

---
+++ models/resetpassword.go
@@ -0,0 +1,14 @@
+package models
+
+import "time"
+
+type PasswordReset struct {
+	Id         int       `db:"id" json:"id"`
+	UserId     int       `db:"user_id" json:"userId"`
+	TokenReset string    `db:"token_reset" json:"tokenReset"`
+	ExpiredAt  time.Time `db:"expired_at" json:"expiredAt"`
+	CreatedAt  time.Time `db:"created_at" json:"createdAt"`
+	UpdatedAt  time.Time `db:"updated_at" json:"updatedAt"`
+	CreatedBy  int       `db:"created_by" json:"createdBy"`
+	UpdatedBy  int       `db:"updated_by" json:"updatedBy"`
+}
