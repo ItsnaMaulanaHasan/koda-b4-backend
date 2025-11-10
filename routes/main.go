5331029091e22fdb25114c4b63c228d0b113421d
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2025-11-10 21:28:33 +0700

    feat: create route for get all transaction

--- routes/main.go
+++ routes/main.go
@@ -12,4 +12,5 @@ func SetUpRoutes(r *gin.Engine) {
 	admin := r.Group("/admin", middlewares.Auth(), middlewares.AdminOnly())
 	SetupUserRoutes(r, admin)
 	SetupProductRoutes(r, admin)
+	SetupTransactionRoutes(r, admin)
 }
---
+++ routes/transaction.go
@@ -0,0 +1,14 @@
+package routes
+
+import (
+	"backend-daily-greens/controllers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func SetupTransactionRoutes(r *gin.Engine, admin *gin.RouterGroup) {
+	transactions := admin.Group("/transactions")
+	{
+		transactions.GET("", controllers.GetAllTransaction)
+	}
+}
