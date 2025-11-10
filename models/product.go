98206602b43443fbaefcf19ab85715d66ecc726c
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2025-11-10 11:33:49 +0700

    feat: create models for product create

--- models/product.go
+++ models/product.go
@@ -1,16 +1,37 @@
 package models
 
+import "mime/multipart"
+
 type Product struct {
-	Id                int      `json:"id" db:"id"`
-	Image             []string `json:"image" db:"image" form:"image"`
-	Name              string   `json:"name" db:"name" form:"name"`
-	Description       string   `json:"description" db:"description" form:"description"`
-	Price             float64  `json:"price" db:"price" form:"price"`
-	DiscountPercent   float64  `json:"discount_percent" db:"discount_percent" form:"discount_percent"`
-	Rating            float64  `json:"rating" db:"rating" form:"rating"`
-	IsFlashSale       bool     `json:"is_flash_sale" db:"is_flash_sale" form:"is_flash_sale"`
-	Stock             int      `json:"stock" db:"stock" form:"stock"`
-	IsActive          bool     `json:"is_active" db:"is_active" form:"is_active"`
-	SizeProducts      []string `json:"size_products" db:"size_products" form:"size_products"`
-	ProductCategories []string `json:"product_categories" db:"product_categories" form:"product_categories"`
+	Id                int      `db:"id"`
+	Images            []string `db:"images"`
+	Name              string   `db:"name"`
+	Description       string   `db:"description"`
+	Price             float64  `db:"price"`
+	DiscountPercent   float64  `db:"discount_percent"`
+	Rating            float64  `db:"rating"`
+	IsFlashSale       bool     `db:"is_flash_sale"`
+	Stock             int      `db:"stock"`
+	IsActive          bool     `db:"is_active"`
+	SizeProducts      []string `db:"size_products"`
+	ProductCategories []string `db:"product_categories"`
+}
+
+type ProductCreateRequest struct {
+	Id                int
+	Image1            *multipart.FileHeader `form:"image1"`
+	Image2            *multipart.FileHeader `form:"image2"`
+	Image3            *multipart.FileHeader `form:"image3"`
+	Image4            *multipart.FileHeader `form:"image4"`
+	Images            []string
+	Name              string  `form:"name"`
+	Description       string  `form:"description"`
+	Price             float64 `form:"price"`
+	DiscountPercent   float64 `form:"discount_percent"`
+	Rating            float64 `form:"rating"`
+	IsFlashSale       bool    `form:"is_flash_sale"`
+	Stock             int     `form:"stock"`
+	IsActive          bool    `form:"is_active"`
+	SizeProducts      string  `form:"size_products"`
+	ProductCategories string  `form:"product_categories"`
 }
