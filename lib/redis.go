6164bd1742f6efa5f87f65bc78bdc4739a02c775
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2025-11-11 13:08:05 +0700

    refactor: move redis url to env

--- lib/redis.go
+++ lib/redis.go
@@ -1,10 +1,14 @@
 package lib
 
-import "github.com/redis/go-redis/v9"
+import (
+	"os"
+
+	"github.com/redis/go-redis/v9"
+)
 
 func Redis() *redis.Client {
 	rdb := redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
+		Addr:     os.Getenv("REDIS_URL"),
 		Password: "",
 		DB:       0,
 		Protocol: 2,
