15aef43a35a934ad5f7876db67ebd9284eb63b6e
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2025-11-11 11:59:07 +0700

    feat: create function connection redis

---
+++ lib/redis.go
@@ -0,0 +1,13 @@
+package lib
+
+import "github.com/redis/go-redis/v9"
+
+func Redis() *redis.Client {
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+		Protocol: 2,
+	})
+	return rdb
+}
