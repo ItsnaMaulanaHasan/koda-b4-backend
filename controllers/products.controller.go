640ad73a52924d12ebca086b0bac2394f3cbfcf8
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2025-11-11 13:46:05 +0700

    feat: create functionality caching in detail product

--- controllers/products.controller.go
+++ controllers/products.controller.go
@@ -20,20 +20,20 @@ import (
 	"github.com/jackc/pgx/v5"
 )
 
-// GetAllProduct godoc
-// @Summary      Get all product
-// @Description  Retrieving all product data with pagination support
-// @Tags         products
-// @Produce      json
-// @Security     BearerAuth
-// @Param        Authorization  header    string  true   "Bearer token" default(Bearer <token>)
-// @Param        page   		query     int     false  "Page number"  default(1)  minimum(1)
-// @Param        limit          query     int     false  "Number of items per page"  default(10)  minimum(1)  maximum(100)
-// @Param        search         query     string  false  "Search value"
-// @Success      200            {object}  object{success=bool,message=string,data=[]models.Product,meta=object{currentPage=int,perPage=int,totalData=int,totalPages=int,next=string,prev=string}}  "Successfully retrieved product list"
-// @Failure      400            {object}  lib.ResponseError  "Invalid pagination parameters or page out of range."
-// @Failure      500            {object}  lib.ResponseError  "Internal server error while fetching or processing product data."
-// @Router       /admin/products [get]
+// ListAllProducts godoc
+// @Summary        Get all product
+// @Description    Retrieving all product data with pagination support
+// @Tags           products
+// @Produce        json
+// @Security       BearerAuth
+// @Param          Authorization  header    string  true   "Bearer token" default(Bearer <token>)
+// @Param          page   		  query     int     false  "Page number"  default(1)  minimum(1)
+// @Param          limit          query     int     false  "Number of items per page"  default(10)  minimum(1)  maximum(100)
+// @Param          search         query     string  false  "Search value"
+// @Success        200            {object}  object{success=bool,message=string,data=[]models.Product,meta=object{currentPage=int,perPage=int,totalData=int,totalPages=int,next=string,prev=string}}  "Successfully retrieved product list"
+// @Failure        400            {object}  lib.ResponseError  "Invalid pagination parameters or page out of range."
+// @Failure        500            {object}  lib.ResponseError  "Internal server error while fetching or processing product data."
+// @Router         /admin/products [get]
 func ListAllProducts(ctx *gin.Context) {
 	page, _ := strconv.Atoi(ctx.DefaultQuery("page", "1"))
 	limit, _ := strconv.Atoi(ctx.DefaultQuery("limit", "10"))
@@ -193,20 +193,20 @@ func ListAllProducts(ctx *gin.Context) {
 	})
 }
 
-// GetProductById   godoc
-// @Summary         Get product by Id
-// @Description     Retrieving product data based on Id
-// @Tags         	products
-// @Accept 		 	x-www-form-urlencoded
-// @Produce      	json
-// @Security     	BearerAuth
-// @Param        	Authorization  header  string  true  "Bearer token"  default(Bearer <token>)
-// @Param        	id   			path    int     true  "product Id"
-// @Success      	200  {object}  lib.ResponseSuccess{data=models.Product}  "Successfully retrieved product"
-// @Failure      	400  {object}  lib.ResponseError  "Invalid Id format"
-// @Failure      	404  {object}  lib.ResponseError  "Product not found"
-// @Failure      	500  {object}  lib.ResponseError  "Internal server error while fetching products from database"
-// @Router       	/admin/products/{id} [get]
+// DetailProduct   godoc
+// @Summary        Get product by Id
+// @Description    Retrieving product data based on Id
+// @Tags           products
+// @Accept 		   x-www-form-urlencoded
+// @Produce        json
+// @Security       BearerAuth
+// @Param          Authorization    header  string  true  "Bearer token"  default(Bearer <token>)
+// @Param          id   			path    int     true  "product Id"
+// @Success        200  {object}  lib.ResponseSuccess{data=models.Product}  "Successfully retrieved product"
+// @Failure        400  {object}  lib.ResponseError  "Invalid Id format"
+// @Failure        404  {object}  lib.ResponseError  "Product not found"
+// @Failure        500  {object}  lib.ResponseError  "Internal server error while fetching products from database"
+// @Router         /admin/products/{id} [get]
 func DetailProduct(ctx *gin.Context) {
 	id, err := strconv.Atoi(ctx.Param("id"))
 	if err != nil {
@@ -218,55 +218,56 @@ func DetailProduct(ctx *gin.Context) {
 		return
 	}
 
-	query := `SELECT 
-				p.id,
-				p.name,
-				p.description,
-				p.price,
-				COALESCE(p.discount_percent, 0) AS discount_percent,
-				COALESCE(p.rating, 0) AS rating,
-				p.is_flash_sale,
-				COALESCE(p.stock, 0) AS stock,
-				p.is_active,
-				COALESCE(ARRAY_AGG(DISTINCT pi.image) FILTER (WHERE pi.image IS NOT NULL), '{}') AS images,
-				COALESCE(ARRAY_AGG(DISTINCT s.name) FILTER (WHERE s.name IS NOT NULL), '{}') AS size_products,
-				COALESCE(ARRAY_AGG(DISTINCT c.name) FILTER (WHERE c.name IS NOT NULL), '{}') AS product_categories
-			FROM products p
-			LEFT JOIN product_images pi ON pi.product_id = p.id
-			LEFT JOIN size_products sp ON sp.product_id = p.id
-			LEFT JOIN sizes s ON s.id = sp.size_id
-			LEFT JOIN product_categories pc ON pc.product_id = p.id
-			LEFT JOIN categories c ON c.id = pc.category_id 
-			WHERE p.id = $1
-			GROUP BY p.id;`
-
-	rows, err := config.DB.Query(context.Background(), query, id)
-	if err != nil {
-		ctx.JSON(http.StatusInternalServerError, lib.ResponseError{
-			Success: false,
-			Message: "Failed to fetch product from database",
-			Error:   err.Error(),
-		})
-		return
-	}
-	defer rows.Close()
-
-	product, err := pgx.CollectOneRow(rows, pgx.RowToStructByName[models.Product])
-	if err != nil {
-		if errors.Is(err, pgx.ErrNoRows) {
-			ctx.JSON(http.StatusNotFound, lib.ResponseError{
+	var product models.Product
+	cache, _ := lib.Redis().Get(context.Background(), "totalDataProduct").Result()
+	if cache == "" {
+		product, err = models.GetProductById(id)
+		if err != nil {
+			if errors.Is(err, pgx.ErrNoRows) {
+				ctx.JSON(http.StatusNotFound, lib.ResponseError{
+					Success: false,
+					Message: "Product not found",
+					Error:   err.Error(),
+				})
+				return
+			}
+			ctx.JSON(http.StatusInternalServerError, lib.ResponseError{
 				Success: false,
-				Message: "Product not found",
+				Message: "Failed in data process product from database",
+				Error:   err.Error(),
 			})
 			return
 		}
 
-		ctx.JSON(http.StatusInternalServerError, lib.ResponseError{
-			Success: false,
-			Message: "Failed to process product data",
-			Error:   err.Error(),
-		})
-		return
+		productStr, err := json.Marshal(product)
+		if err != nil {
+			ctx.JSON(http.StatusInternalServerError, lib.ResponseError{
+				Success: false,
+				Message: "Failed to serialization detail product",
+				Error:   err.Error(),
+			})
+			return
+		}
+
+		err = lib.Redis().Set(context.Background(), ctx.Request.RequestURI, productStr, 60*time.Second).Err()
+		if err != nil {
+			ctx.JSON(http.StatusInternalServerError, lib.ResponseError{
+				Success: false,
+				Message: "Failed to set data product to cache",
+				Error:   err.Error(),
+			})
+			return
+		}
+	} else {
+		err = json.Unmarshal([]byte(cache), &product)
+		if err != nil {
+			ctx.JSON(http.StatusInternalServerError, lib.ResponseError{
+				Success: false,
+				Message: "Failed to unmarshal data products from cache",
+				Error:   err.Error(),
+			})
+			return
+		}
 	}
 
 	ctx.JSON(http.StatusOK, lib.ResponseSuccess{
