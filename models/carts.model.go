f295c7fccfcc0e55dff57740e8c58a372ddfb435
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2025-11-12 14:27:37 +0700

    feat: added tag struct for json

--- models/carts.model.go
+++ models/carts.model.go
@@ -8,14 +8,14 @@ import (
 )
 
 type Cart struct {
-	Id            int      `db:"id"`
-	UserId        int      `db:"user_id"`
-	ProductImages []string `db:"product_images"`
-	ProductName   string   `db:"product_name"`
-	SizeName      string   `db:"size_name"`
-	VariantName   string   `db:"variant_name"`
-	Amount        int      `db:"amount"`
-	Subtotal      float64  `db:"subtotal"`
+	Id            int      `db:"id" json:"id"`
+	UserId        int      `db:"user_id" json:"userId"`
+	ProductImages []string `db:"product_images" json:"productImages"`
+	ProductName   string   `db:"product_name" json:"productName"`
+	SizeName      string   `db:"size_name" json:"sizeName"`
+	VariantName   string   `db:"variant_name" json:"variantName"`
+	Amount        int      `db:"amount" json:"amount"`
+	Subtotal      float64  `db:"subtotal" json:"subtotal"`
 }
 
 type CartRequest struct {
