fb0cac6114f696db736167b8848caf5e10bacc42
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2025-11-12 10:34:31 +0700

    feat: create base model and controller for carts

---
+++ controllers/carts.controller.go
@@ -0,0 +1,19 @@
+package controllers
+
+import "github.com/gin-gonic/gin"
+
+func ListCarts(ctx *gin.Context) {
+
+}
+
+func AddCart(ctx *gin.Context) {
+
+}
+
+func UpdateCart(ctx *gin.Context) {
+
+}
+
+func DeleteCart(ctx *gin.Context) {
+
+}
