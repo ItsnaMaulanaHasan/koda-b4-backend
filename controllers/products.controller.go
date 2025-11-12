a9993445e8b1a9c37a25916260a8c6afd68164b7
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2025-11-12 09:03:38 +0700

    feat: adjust switch case for hateoas on listproduct admin

--- controllers/products.controller.go
+++ controllers/products.controller.go
@@ -1152,11 +1152,14 @@ func ListProductsPublic(ctx *gin.Context) {
 
 	var next any
 	var prev any
-	switch page {
-	case 1:
+	switch {
+	case totalData == 0:
+		page = 0
+		next, prev = nil, nil
+	case page == 1 && totalPage > 1:
 		next = fmt.Sprintf("%s?page=%v&limit=%v", baseURL, page+1, limit)
 		prev = nil
-	case totalPage:
+	case page == totalPage && totalPage > 1:
 		next = nil
 		prev = fmt.Sprintf("%s?page=%v&limit=%v", baseURL, page-1, limit)
 	default:
