e39519ced7a14fec9f243cb84fa4c94a9fa9827c
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2025-11-12 11:33:25 +0700

    feat: create model for cart

--- models/carts.model.go
+++ models/carts.model.go
@@ -1,1 +1,21 @@
 package models
+
+type Cart struct {
+	Id          int     `db:"id"`
+	UserId      int     `db:"user_id"`
+	ProductName string  `db:"product_name"`
+	Size        string  `db:"size"`
+	Variant     string  `db:"variant"`
+	Amount      int     `db:"amount"`
+	Subtotal    float64 `db:"subtotal"`
+}
+
+type CartRequest struct {
+	Id        int     `json:"-"`
+	UserId    int     `json:"-"`
+	ProductId int     `json:"productId"`
+	SizeId    int     `json:"sizeId"`
+	VariantId int     `json:"variantId"`
+	Amount    float64 `json:"amount"`
+	Subtotal  float64 `json:"subtotal"`
+}
