cff748a9a620b406fcb52daeb75bc038b6de32d2
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2025-11-13 18:05:01 +0700

    feat: created testing redis and adjust connection redis

--- lib/redis.go
+++ lib/redis.go
@@ -1,6 +1,9 @@
 package lib
 
 import (
+	"context"
+	"crypto/tls"
+	"log"
 	"os"
 
 	"github.com/redis/go-redis/v9"
@@ -8,10 +11,18 @@ import (
 
 func Redis() *redis.Client {
 	rdb := redis.NewClient(&redis.Options{
-		Addr:     os.Getenv("REDIS_CLIENT"),
-		Password: os.Getenv("REDIS_PASSWORD"),
-		DB:       0,
-		Protocol: 2,
+		Addr:      os.Getenv("REDIS_CLIENT"),
+		Password:  os.Getenv("REDIS_PASSWORD"),
+		DB:        0,
+		TLSConfig: &tls.Config{},
 	})
+
+	ctx := context.Background()
+	if err := rdb.Ping(ctx).Err(); err != nil {
+		log.Fatalf("Redis connection failed: %v", err)
+	} else {
+		log.Println("Connected to Redis successfully!")
+	}
+
 	return rdb
 }
