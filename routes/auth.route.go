2f24c0f392e41aa188a0df0a48565a44b915ccee
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2025-11-17 11:34:11 +0700

    feat: create route for logout

--- routes/auth.route.go
+++ routes/auth.route.go
@@ -9,6 +9,7 @@ import (
 func authRouter(r *gin.RouterGroup) {
 	r.POST("/register", controllers.Register)
 	r.POST("/login", controllers.Login)
+	r.POST("/logout", controllers.Logout)
 	r.POST("/forgot-password", controllers.GetTokenReset)
 	r.POST("/verify-reset-token", controllers.VerifyResetToken)
 	r.PATCH("/reset-password", controllers.ResetPassword)
