bf193df356bd363899dbe423e98b7bf07fc03cd8
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2025-11-17 13:13:57 +0700

    fix: adjust logout

--- routes/auth.route.go
+++ routes/auth.route.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"backend-daily-greens/controllers"
+	"backend-daily-greens/middlewares"
 
 	"github.com/gin-gonic/gin"
 )
@@ -9,8 +10,8 @@ import (
 func authRouter(r *gin.RouterGroup) {
 	r.POST("/register", controllers.Register)
 	r.POST("/login", controllers.Login)
-	r.POST("/logout", controllers.Logout)
 	r.POST("/forgot-password", controllers.GetTokenReset)
 	r.POST("/verify-reset-token", controllers.VerifyResetToken)
 	r.PATCH("/reset-password", controllers.ResetPassword)
+	r.POST("/logout", middlewares.Auth(), controllers.Logout)
 }
