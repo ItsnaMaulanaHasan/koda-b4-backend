e68d7a0b3956ca98224c766e6ef91abe929747e0
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2025-11-21 01:56:33 +0700

    feat: create function handler for reset password

--- lib/email.go
+++ lib/email.go
@@ -1,21 +1,60 @@
 package lib
 
 import (
+	"fmt"
+	"net/smtp"
 	"os"
-
-	resend "github.com/resend/resend-go/v2"
 )
 
-func SendResendEmail(toEmail, subject, htmlBody string) error {
-	client := resend.NewClient(os.Getenv("RESEND_API_KEY"))
+type EmailConfig struct {
+	SMTPHost     string
+	SMTPPort     string
+	SMTPUsername string
+	SMTPPassword string
+	FromEmail    string
+}
 
-	params := &resend.SendEmailRequest{
-		From:    os.Getenv("EMAIL_FROM"),
-		To:      []string{toEmail},
-		Subject: subject,
-		Html:    htmlBody,
+func SendPasswordResetEmail(toEmail, token string) error {
+	config := EmailConfig{
+		SMTPHost:     os.Getenv("SMTP_HOST"),
+		SMTPPort:     os.Getenv("SMTP_PORT"),
+		SMTPUsername: os.Getenv("SMTP_USERNAME"),
+		SMTPPassword: os.Getenv("SMTP_PASSWORD"),
+		FromEmail:    os.Getenv("FROM_EMAIL"),
 	}
 
-	_, err := client.Emails.Send(params)
-	return err
+	if config.SMTPHost == "" || config.SMTPPort == "" || config.SMTPUsername == "" || config.SMTPPassword == "" {
+		return fmt.Errorf("SMTP configuration is incomplete")
+	}
+
+	auth := smtp.PlainAuth("", config.SMTPUsername, config.SMTPPassword, config.SMTPHost)
+
+	resetLink := fmt.Sprintf("http://165.22.110.38:9032/reset-password?email=%s&token=%s", toEmail, token)
+
+	subject := "Subject: Password Reset Request\r\n"
+	mime := "MIME-version: 1.0;\r\nContent-Type: text/html; charset=\"UTF-8\";\r\n\r\n"
+
+	body := fmt.Sprintf(`
+		<html>
+			<body>
+				<h2>Password Reset Request</h2>
+				<p>You have requested to reset your password. Click the link below to reset your password:</p>
+				<p><a href="%s">Reset Password</a></p>
+				<p>Or copy and paste this link into your browser:</p>
+				<p>%s</p>
+				<p>This link will expire in 1 hour.</p>
+				<p>If you did not request this, please ignore this email.</p>
+			</body>
+		</html>
+	`, resetLink, resetLink)
+
+	message := []byte(subject + mime + body)
+
+	addr := fmt.Sprintf("%s:%s", config.SMTPHost, config.SMTPPort)
+	err := smtp.SendMail(addr, auth, config.FromEmail, []string{toEmail}, message)
+	if err != nil {
+		return fmt.Errorf("failed to send email: %v", err)
+	}
+
+	return nil
 }
