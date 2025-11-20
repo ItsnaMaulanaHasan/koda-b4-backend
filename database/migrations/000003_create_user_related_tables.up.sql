44fa1b187f8c70f925ae0e019112b8068a169008
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2025-11-20 16:55:15 +0700

    feat: update struktur db

--- database/migrations/000003_create_user_related_tables.up.sql
+++ database/migrations/000003_create_user_related_tables.up.sql
@@ -29,7 +29,7 @@ CREATE TABLE "sessions" (
 CREATE TABLE "password_resets" (
     "id" serial PRIMARY KEY,
     "user_id" int,
-    "token_reset" char(6) UNIQUE NOT NULL,
+    "token_reset" char(12) UNIQUE NOT NULL,
     "expired_at" timestamp NOT NULL DEFAULT (
         CURRENT_TIMESTAMP + INTERVAL '1 hour'
     ),
