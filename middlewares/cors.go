93fd6c6b76c0c5a14590c6ab08cbbc3d66da8741
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2025-11-20 10:33:41 +0700

    feat: adjust cors origin and rename file preflight

--- middlewares/cors.go
+++ middlewares/cors.go
@@ -3,14 +3,16 @@ package middlewares
 import (
 	"os"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func CorsMiddleware() gin.HandlerFunc {
-	return func(ctx *gin.Context) {
-		ctx.Header("Access-Control-Allow-Origin", os.Getenv("ORIGIN_URL"))
-		ctx.Header("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS")
-		ctx.Header("Access-Control-Allow-Headers", "Content-Type")
-		ctx.Next()
-	}
+	return cors.New(cors.Config{
+		AllowOrigins:     []string{os.Getenv("ORIGIN_URL"), "http://localhost:5173"},
+		AllowMethods:     []string{"PATCH", "POST", "GET", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Content-Type", "Authorization"},
+		AllowCredentials: true,
+		MaxAge:           12 * 3600,
+	})
 }
