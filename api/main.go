eaeee201b61cde4d78dc8473f2841405dc466f9a
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2025-11-23 09:44:34 +0700

    test: testing deployment vercel

--- api/main.go
+++ api/main.go
@@ -8,6 +8,7 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"sync"
 
 	_ "backend-daily-greens/docs"
 
@@ -16,31 +17,38 @@ import (
 	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
-var App *gin.Engine
+var (
+	App  *gin.Engine
+	once sync.Once
+)
 
-func init() {
-	fmt.Println("ORIGIN_URL:", os.Getenv("ORIGIN_URL"))
-	fmt.Println("ORIGIN_URL_VERCEL:", os.Getenv("ORIGIN_URL_VERCEL"))
-	fmt.Println("ORIGIN_URL_VERCEL2:", os.Getenv("ORIGIN_URL_VERCEL2"))
-	App = gin.New()
-	App.Use(gin.Recovery())
-	App.Use(middlewares.CorsMiddleware())
+func initApp() {
+	once.Do(func() {
+		fmt.Println("ORIGIN_URL:", os.Getenv("ORIGIN_URL"))
+		fmt.Println("ORIGIN_URL_VERCEL:", os.Getenv("ORIGIN_URL_VERCEL"))
+		fmt.Println("ORIGIN_URL_VERCEL2:", os.Getenv("ORIGIN_URL_VERCEL2"))
 
-	App.GET("/", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, lib.ResponseSuccess{
-			Success: true,
-			Message: "Backend is running well",
+		App = gin.New()
+		App.Use(gin.Recovery())
+		App.Use(middlewares.CorsMiddleware())
+
+		App.GET("/", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, lib.ResponseSuccess{
+				Success: true,
+				Message: "Backend is running well",
+			})
 		})
+
+		App.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+		App.MaxMultipartMemory = 1 << 20
+
+		routes.SetUpRoutes(App)
 	})
-
-	App.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
-
-	App.MaxMultipartMemory = 1 << 20
-
-	routes.SetUpRoutes(App)
 }
 
 func Handler(w http.ResponseWriter, r *http.Request) {
+	initApp()
+
 	config.InitDatabase()
 	config.InitRedis()
 	config.InitSupabase()
